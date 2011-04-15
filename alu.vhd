36e86741d7743c35b1be3d59d798215bba9ed77c
Themaister <maister@archlinux.us>
2011-04-15 15:52:45 +0200

    Clean up ALU/display. Remove more unneeded cruft.

--- alu.vhd
+++ alu.vhd
@@ -6,8 +6,11 @@
 --------------------------------------------------
 
 --------------------------------------------------
--- Extensions by Hans-Kristian Arntzen
+-- (Lots of) extensions by Hans-Kristian Arntzen
 --------------------------------------------------
+
+-- Not the most efficient ALU for sure. We do some awkward stuff here like jr/jalr logic. 
+-- The MUL/DIV register logic also resides here.
 
 library ieee; 
 use ieee.std_logic_1164.all;
@@ -15,21 +18,23 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity alu is
-   port(clk:        in       std_logic; -- for mul/div reg
-   a, b:       in       std_logic_vector(31 downto 0);
-   f:          in       std_logic_vector(7 downto 0); --sll - 6 to 4 mul/div, 7 unsigned
-   shamt:      in       std_logic_vector(4 downto 0); --sll
-   alu_out:    out      std_logic_vector(31 downto 0);
-   zero:       inout    std_logic;  --blez
-   ltez:       out      std_logic; --blez
-   jr:         out      std_logic; --jr
-   link:       out      std_logic; --jalr
-   write_reg:  out      std_logic
-); --mul/div nor jr 
+   port
+   (
+      clk : in std_logic; -- for mul/div reg
+      a, b : in std_logic_vector(31 downto 0);
+      f : in std_logic_vector(7 downto 0); -- ALU function. A bit hacky. bits 0-3 for general stuff, 4-6 for mul/div ops, 7 for unsigned op.
+      shamt : in std_logic_vector(4 downto 0); -- Shifts
+      alu_out : out std_logic_vector(31 downto 0); -- Final result
+      zero : inout std_logic;  -- Branching
+      ltez : out std_logic; -- Branching
+      jr : out std_logic; -- jr
+      link : out std_logic; -- jalr
+      write_reg : out std_logic -- Can we write to register? Not always the case, due to jr or mul/div.
+   );
 end;
 
 architecture synth of alu is
-   signal s, bout:      std_logic_vector(31 downto 0);
+   signal s, bout : std_logic_vector(31 downto 0);
    signal mul_div_write_op : std_logic;
    signal jump_reg : std_logic;
    signal can_link : std_logic;
@@ -42,47 +47,53 @@ architecture synth of alu is
    signal slt : std_logic;
 
    component mul_div_reg is
-      port (
-              clk : in std_logic;
-              we_hi : in std_logic;
-              we_lo : in std_logic;
-              hi : in std_logic_vector(31 downto 0);
-              lo : in std_logic_vector(31 downto 0);
+      port 
+      (
+         clk : in std_logic;
+         we_hi : in std_logic;
+         we_lo : in std_logic;
+         hi : in std_logic_vector(31 downto 0);
+         lo : in std_logic_vector(31 downto 0);
 
-              read_hi_lo : in std_logic;
+         read_hi_lo : in std_logic;
 
-              output : out std_logic_vector(31 downto 0)
-           );
+         output : out std_logic_vector(31 downto 0)
+      );
    end component;
 
-   component mux2 is -- two-input multiplexer
+   component mux2 is
       generic(width: integer);
-      port(d0, d1: in  std_logic_vector(width-1 downto 0);
-           s:      in  std_logic;
-           y:      out std_logic_vector(width-1 downto 0));
+      port
+      (
+         d0, d1: in  std_logic_vector(width-1 downto 0);
+         s:      in  std_logic;
+         y:      out std_logic_vector(width-1 downto 0)
+      );
    end component;
    
    component divider is
-   port 
-   (
-	 num : in std_logic_vector(31 downto 0);
-	 denom : in std_logic_vector(31 downto 0);
-	 quotient : out std_logic_vector(31 downto 0);
-	 remainder : out std_logic_vector(31 downto 0);
-	 use_unsigned : in std_logic
-   );  
+      port 
+      (
+         num : in std_logic_vector(31 downto 0);
+         denom : in std_logic_vector(31 downto 0);
+         quotient : out std_logic_vector(31 downto 0);
+         remainder : out std_logic_vector(31 downto 0);
+         use_unsigned : in std_logic
+      );  
    end component;
    
    component mips_multiplier is
-	port (
-		a : in std_logic_vector(31 downto 0);
-		b : in std_logic_vector(31 downto 0);
-		output : out std_logic_vector(63 downto 0);
-		is_unsigned : in std_logic);
-	end component;
+      port 
+      (
+         a : in std_logic_vector(31 downto 0);
+         b : in std_logic_vector(31 downto 0);
+         output : out std_logic_vector(63 downto 0);
+         is_unsigned : in std_logic
+      );
+   end component;
 begin
 
-   -- HI/LO register for use with mul/div instructions, and the "never used"-mtlo,mthi.
+   -- HI/LO register for use with mul/div instructions, and the "never used" - mtlo, mthi.
    muldivreg_1 : mul_div_reg 
    port map (clk, we_hi, we_lo, 
       alu_res(63 downto 32), alu_res(31 downto 0),
@@ -94,98 +105,74 @@ begin
       read_mul_div_reg, alu_out 
    );
    
-   -- single cycle divider megafunction. This is our critical path.
+   -- Single cycle divider megafunction. This and MUL is our critical path (it's insane ...).
    divider_1 : divider
    port map (a, b, divider_quot, divider_rem, f(7));
    
-   -- single cycle multiplier megafunction. Does signed and unsigned.
+   -- Single cycle multiplier megafunction. Does signed and unsigned.
    multiplier_1 : mips_multiplier port map(a, b, mult_res, f(7));
-
 
    bout <= (not b) when (f(3) = '1') else b;
    s <= a + bout + f(3);
 
 
-  -- alu function
-process (f, a, bout, s) begin
-   case f(6 downto 0) is
-      when "0000000" => 
-         alu_res <= x"00000000" & (a and bout);
-      when "0000001" => 
-         alu_res <= x"00000000" & (a or bout);
-      when "0001001" =>
-         alu_res <= x"00000000" & (a xor (not bout));
-      when "0001111" =>
-         alu_res <= x"00000000" & (a nor (not bout));
-      when "0001010" => 
-         alu_res <= x"00000000" & s;
-      when "0000010" =>
-         alu_res <= x"00000000" & s;
-      when "0001011" => 
-         alu_res <= conv_std_logic_vector(0, 63) & slt; --slt/sltu
+   process (f, a, b, bout, s) begin
+      case f(6 downto 0) is
 
-     -- shifting
-       when "0000100" => 
-            alu_res <= 
-               x"00000000" & to_stdlogicvector(to_bitvector(b) sll conv_integer(shamt)); --sll
-       when "0000101" => 
-            alu_res <=
-               x"00000000" & to_stdlogicvector(to_bitvector(b) srl conv_integer(shamt)); --srl
-       when "0000110" => 
-           alu_res <=
-          x"00000000" & to_stdlogicvector(to_bitvector(b) sra conv_integer(shamt)); --sra
-       when "0001100" => 
-             alu_res <= 
-                 x"00000000" & to_stdlogicvector(to_bitvector(b) sll conv_integer(a(4 downto 0))); --sllv
-       when "0001101" => 
-             alu_res <=
-               x"00000000" & to_stdlogicvector(to_bitvector(b) srl conv_integer(a(4 downto 0))); --srlv
-       when "0001110" => 
-            alu_res <=
-              x"00000000" & to_stdlogicvector(to_bitvector(b) sra conv_integer(a(4 downto 0))); --srav
+         -- Basic ALU stuff.
+         when "0000000" => alu_res <= x"00000000" & (a and bout);
+         when "0000001" => alu_res <= x"00000000" & (a or b);
+         when "0001001" => alu_res <= x"00000000" & (a xor b);
+         when "0001111" => alu_res <= x"00000000" & (a nor b);
+         when "0001010" => alu_res <= x"00000000" & s;
+         when "0000010" => alu_res <= x"00000000" & s;
+         when "0001011" => alu_res <= conv_std_logic_vector(0, 63) & slt; -- slt/sltu
 
-         -- end shifting
-       when "1100000" => 
-           alu_res <= mult_res; -- Multiplier
+          -- Shifting
+         when "0000100" => alu_res <= x"00000000" & to_stdlogicvector(to_bitvector(b) sll conv_integer(shamt)); -- sll
+         when "0000101" => alu_res <= x"00000000" & to_stdlogicvector(to_bitvector(b) srl conv_integer(shamt)); -- srl
+         when "0000110" => alu_res <= x"00000000" & to_stdlogicvector(to_bitvector(b) sra conv_integer(shamt)); -- sra
+         when "0001100" => alu_res <= x"00000000" & to_stdlogicvector(to_bitvector(b) sll conv_integer(a(4 downto 0))); -- sllv
+         when "0001101" => alu_res <= x"00000000" & to_stdlogicvector(to_bitvector(b) srl conv_integer(a(4 downto 0))); -- srlv
+         when "0001110" => alu_res <= x"00000000" & to_stdlogicvector(to_bitvector(b) sra conv_integer(a(4 downto 0))); -- srav
 
-       when "1110000" => 
-          alu_res <= divider_rem & divider_quot; -- Divider
-          
+         when "1100000" => alu_res <= mult_res; -- Multiplier
+         when "1110000" => alu_res <= divider_rem & divider_quot; -- Divider
 
-       when "1000000" =>
-          alu_res <= a & x"00000000";
-       when "1010000" =>
-          alu_res <= x"00000000" & a;
+         when "1000000" => alu_res <= a & x"00000000"; -- mthi
+         when "1010000" => alu_res <= x"00000000" & a; -- mtlo
 
-       when others => 
-          alu_res(63 downto 0) <= x"0000000000000000";
-   end case;
-end process;
+         when others => alu_res(63 downto 0) <= conv_std_logic_vector(0, 64);
 
- zero <= '1' when (alu_res(31 downto 0) = x"00000000") else '0'; -- beq/bne
- jump_reg <= '1' when (f(3 downto 0) = "1000") else '0';
- jr <= jump_reg;
- can_link <= '0' when (a = x"00000000") else '1';
- link <= jump_reg and can_link;
- 
+      end case;
+   end process;
 
- mul_div_write_op <= f(6);
- we_hi <= f(6) and (not (f(5) xor f(4)));
- we_lo <= f(6) and (f(5) or f(4));
- read_mul_div_reg <= f(5) or f(4);
- read_hi_lo <= f(4);
+   zero <= '1' when (alu_res(31 downto 0) = x"00000000") else '0'; -- beq/bne
+   jump_reg <= '1' when (f(3 downto 0) = "1000") else '0';
+   jr <= jump_reg;
+   can_link <= '0' when (a = x"00000000") else '1';
+   link <= jump_reg and can_link;
 
- write_reg <= (jump_reg and not can_link) nor mul_div_write_op;
- ltez <= zero or s(31);  -- blez/bgtz
- 
- -- Calculate SLT, if unsigned we have to do some additional checks.
- process (s(31), f(7), a(31), b(31)) begin
-    if f(7) = '0' then
-		slt <= s(31);
-	else
-		slt <= ((not (a(31) xor b(31))) and s(31)) or (a(31) and (not b(31)));
-	end if;
- end process;
+   -- Not very pretty logic ... ;) But works.
+   mul_div_write_op <= f(6);
+   we_hi <= f(6) and (not (f(5) xor f(4)));
+   we_lo <= f(6) and (f(5) or f(4));
+   read_mul_div_reg <= f(5) or f(4);
+   read_hi_lo <= f(4);
 
+   write_reg <= (jump_reg and not can_link) nor mul_div_write_op;
+   ltez <= zero or s(31);  -- blez/bgtz
+
+    -- Calculate SLT, if unsigned we have to do some additional checks.
+   process (s(31), f(7), a(31), b(31))
+   begin
+
+      if f(7) = '0' then
+         slt <= s(31);
+      else
+         slt <= ((not (a(31) xor b(31))) and s(31)) or (a(31) and (not b(31)));
+      end if;
+
+   end process;
 end;
 
--- display.vhd
+++ display.vhd
@@ -7,19 +7,22 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-
 entity ssd_32bit is
-   port(ssd_in_32bit   : in std_logic_vector (31 downto 0);
-   ssd0, ssd1, ssd2, ssd3, ssd4, ssd5, ssd6, ssd7  : out std_logic_vector (6 downto 0)); 
-end ssd_32bit;
+   port
+   (
+      ssd_in_32bit : in std_logic_vector (31 downto 0);
+      ssd0, ssd1, ssd2, ssd3, ssd4, ssd5, ssd6, ssd7 : out std_logic_vector (6 downto 0)
+   ); 
+end;
 
-architecture ssd_32bit_beh of ssd_32bit is
-
+architecture synth of ssd_32bit is
    component ssd is
-   port(ssd_in   : in std_logic_vector (3 downto 0);
-        ssd_out  : out std_logic_vector (6 downto 0)); 
+      port
+      (
+         ssd_in : in std_logic_vector (3 downto 0);
+         ssd_out : out std_logic_vector (6 downto 0)
+      ); 
    end component;
-
 begin              
    ssd0disp : ssd port map( ssd_in_32bit(3 downto 0), ssd0);
    ssd1disp : ssd port map( ssd_in_32bit(7 downto 4), ssd1);
@@ -29,10 +32,7 @@ begin
    ssd5disp : ssd port map( ssd_in_32bit(23 downto 20), ssd5);
    ssd6disp : ssd port map( ssd_in_32bit(27 downto 24), ssd6);
    ssd7disp : ssd port map( ssd_in_32bit(31 downto 28), ssd7);
-
-end ssd_32bit_beh;
-
-
+end;
 
 --------------------------------------------------
 -- seven segment decoder
@@ -43,13 +43,15 @@ end ssd_32bit_beh;
 
 library ieee;
 use ieee.std_logic_1164.all;
-
 entity ssd is
-   port(ssd_in   : in std_logic_vector (3 downto 0);
-        ssd_out  : out std_logic_vector (6 downto 0)); 
-end ssd;
+   port
+   (
+      ssd_in : in std_logic_vector (3 downto 0);
+      ssd_out : out std_logic_vector (6 downto 0)
+   ); 
+end;
 
-architecture ssd_beh of ssd is
+architecture synth of ssd is
 begin
    process (ssd_in)
    begin
@@ -73,298 +75,6 @@ begin
          when others => ssd_out <= not "1000000"; -- -              
       end case;
    end process;
-end ssd_beh;
+end;
 
 
---------------------------------------------------
--- lcd char converter
---
--- input: 4 bit signal
--- output 8 bit signal
---------------------------------------------------
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity lcdcharconverter is
-   port( char    : in std_logic_vector(3 downto 0);
-         charlcd : out std_logic_vector(7 downto 0));
-end lcdcharconverter;
-
-architecture lcdcharconverter_beh of lcdcharconverter is
-begin
-   process (char)
-   begin
-      case char is
-         when "0000" => charlcd <= x"30"; -- 0
-         when "0001" => charlcd <= x"31"; -- 1
-         when "0010" => charlcd <= x"32"; -- 2
-         when "0011" => charlcd <= x"33"; -- 3
-         when "0100" => charlcd <= x"34"; -- 4
-         when "0101" => charlcd <= x"35"; -- 5
-         when "0110" => charlcd <= x"36"; -- 6
-         when "0111" => charlcd <= x"37"; -- 7
-         when "1000" => charlcd <= x"38"; -- 8
-         when "1001" => charlcd <= x"39"; -- 9
-         when "1010" => charlcd <= x"41"; -- a
-         when "1011" => charlcd <= x"42"; -- b
-         when "1100" => charlcd <= x"43"; -- c
-         when "1101" => charlcd <= x"44"; -- d
-         when "1110" => charlcd <= x"45"; -- e
-         when "1111" => charlcd <= x"46"; -- f
-         when others => charlcd <= x"a0"; -- -
-      end case;
-   end process;
-end lcdcharconverter_beh;
-
-
-
-
-
----------------------------------------------------
--- lcd driver module for driving hd44780 controller
--- a. greensted, june 2007
---
--- modified by jorgen k dohlie, january 2010
---
-
--- generic ticknum must be set such that:
--- ticknum = 10us / period clk
--- this provides an internal tick every 10us
--- clk: 100 mhz, ticknum: 1000
--- clk: 32 mhz, ticknum: 320
--- clk: 10 mhz, ticknum: 100
--- clk: 50 mhz, ticknum 500
----------------------------------------------------
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity lcddriver is
-   generic ( ticknum    : positive := 500);
-   port    ( clk        : in    std_logic; 			-- de2: clock_50
-             reset       : in    std_logic;			-- de2: optional. can use key0 (remember not)
-
-             din32bit    : in    std_logic_vector(31 downto 0);	-- dataadr
-             din32bit2   : in    std_logic_vector(31 downto 0);	-- writedata
-             wen         : in    std_logic;			-- always '1'
-
-            -- lcd interface
-             lcddata     : out   std_logic_vector(7 downto 0);	-- de2: lcd_data		
-             lcdrs       : out   std_logic;			-- de2: lcd_rs
-             lcdrw       : out   std_logic;			-- de2: lcd_rw
-             lcde        : out   std_logic);			-- de2: lcd_en
-
-end lcddriver;
-
-architecture structural of lcddriver is
-
-   component lcdcharconverter is
-   port(char    : in std_logic_vector(3 downto 0);
-        charlcd : out std_logic_vector(7 downto 0));
-   end component;  
-
-
-   -- lcd interface constants
-   constant lcd_read      : std_logic := '1';
-   constant lcd_write     : std_logic := '0';
-   constant data_code     : std_logic := '1';
-   constant insn_code     : std_logic := '0';
-
-   -- tick generation
-   subtype tick_counter_type is integer range 0 to ticknum;
-   signal tick            : std_logic;
-
-   constant warmup_delay  : integer := 2000;  -- 2000: 20ms
-   constant init_delay    : integer := 500;   -- 500:  5ms
-   constant char_delay    : integer := 10;    -- 10:   100us
-
-   subtype delay_type is integer range 0 to warmup_delay;
-   signal timer           : delay_type;
-
-   type init_rom_type is array (0 to 6) of std_logic_vector(7 downto 0);
-   constant initrom       : init_rom_type := (  b"0011_0000",  -- init
-   b"0011_0000",  -- init
-   b"0011_0000",  -- init
-   b"0011_1000",  -- function set: 8 bit, 2 lines, 5x7 characters
-   b"0000_1100",  -- display on/off control: display on, cursor off, blink off
-   b"0000_0001",  -- clear display: move cursor to home
-   b"0000_0110"); -- entry mode set: auto increment cursor, don't shift display
-
-   type char_ram_type is array(0 to 39) of std_logic_vector(7 downto 0);
-   signal charram         : char_ram_type := (   0=>x"44", 1=>x"61", 2=>x"74", 3=>x"61", 4=>x"61", 5=>x"64", 6=>x"72",
-   20=>x"57", 21=>x"72", 22=>x"74", 23=>x"64", 24=>x"61", 25=>x"74", 26=>x"61",
-   others=>x"a0");
-
-   signal setline         : std_logic;
-   signal linenum         : integer range 0 to 1;
-   signal initialising    : std_logic;
-
-   signal initrompointer  : integer range 0 to init_rom_type'high;
-   signal charrampointer  : integer range 0 to char_ram_type'high;
-
-   type state_type is (warmup, stage1, stage2, stage3, delay);
-   signal state           : state_type;
-
-begin
-
---process (charram)
---begin
-
-   conv_char0 : lcdcharconverter port map(din32bit(3 downto 0), charram(15));
-   conv_char1 : lcdcharconverter port map(din32bit(7 downto 4), charram(14));
-   conv_char2 : lcdcharconverter port map(din32bit(11 downto 8), charram(13));
-   conv_char3 : lcdcharconverter port map(din32bit(15 downto 12), charram(12));
-   conv_char4 : lcdcharconverter port map(din32bit(19 downto 16), charram(11));
-   conv_char5 : lcdcharconverter port map(din32bit(23 downto 20), charram(10));
-   conv_char6 : lcdcharconverter port map(din32bit(27 downto 24), charram(9));
-   conv_char7 : lcdcharconverter port map(din32bit(31 downto 28), charram(8));
-
-   conv_char8 : lcdcharconverter port map(din32bit2(3 downto 0), charram(35));
-   conv_char9 : lcdcharconverter port map(din32bit2(7 downto 4), charram(34));
-   conv_char10 : lcdcharconverter port map(din32bit2(11 downto 8), charram(33));
-   conv_char11 : lcdcharconverter port map(din32bit2(15 downto 12), charram(32));
-   conv_char12 : lcdcharconverter port map(din32bit2(19 downto 16), charram(31));
-   conv_char13 : lcdcharconverter port map(din32bit2(23 downto 20), charram(30));
-   conv_char14 : lcdcharconverter port map(din32bit2(27 downto 24), charram(29));
-   conv_char15 : lcdcharconverter port map(din32bit2(31 downto 28), charram(28));
-
-
-   lcdrw <= lcd_write;
-
-   tickgen : process(clk)
-      variable tickcounter : tick_counter_type;
-   begin
-      if (clk'event and clk='1') then
-         if (tickcounter = 0) then
-            tickcounter := tick_counter_type'high-1;
-            tick <= '1';
-         else
-            tickcounter := tickcounter - 1;
-            tick <= '0';
-         end if;
-      end if;
-   end process;
-
---charramwrite : process(clk)
---   variable add : integer range 0 to 39;
---begin
---   if (clk'event and clk='1') then
---      if (wen='1') then
---         add := to_integer(unsigned(charnum));
---         charram(add) <= din;
---      end if;
---   end if;
---end process;
-
-   controller : process (clk)
-   begin
-      if (clk'event and clk='1') then
-
-         if (reset='1') then
-            timer          <= warmup_delay;
-            initrompointer <= 0;
-            charrampointer <= 0;
-
-            lcdrs          <= insn_code;
-            lcde           <= '0';
-            lcddata        <= (others => '0');
-
-            initialising   <= '1';
-            setline        <= '0';
-            linenum        <= 0;
-            state          <= warmup;
-
-         elsif (tick='1') then
-
-            case state is
-
-            -- perform initial long warmup delay
-               when warmup =>
-                  if (timer=0) then
-                     state <= stage1;
-                  else
-                     timer <= timer - 1;
-                  end if;
-
-            -- set the lcd data
-            -- set the lcd rs
-            -- initialise the timer with the required delay
-               when stage1 =>
-                  if (initialising='1') then
-                     timer    <= init_delay;
-                     lcdrs    <= insn_code;
-                     lcddata  <= initrom(initrompointer);
-
-                  elsif (setline='1') then
-                     timer    <= char_delay;
-                     lcdrs    <= insn_code;
-                     case linenum is
-                        when 0 => lcddata   <= b"1000_0000"; -- x00
-                        when 1 => lcddata   <= b"1100_0000"; -- x40
-                     end case;
-
-                  else
-                     timer    <= char_delay;
-                     lcdrs    <= data_code;
-                     lcddata  <= charram(charrampointer);
-
-                  end if;
-
-                  state <= stage2;
-
-            -- set lcde (latching rs and rw)
-               when stage2 =>
-                  if (initialising='1') then
-                     if (initrompointer=init_rom_type'high) then
-                        initialising <= '0';
-                     else
-                        initrompointer <= initrompointer + 1;
-                     end if;
-
-                  elsif (setline='1') then
-                     setline <= '0';
-
-                  else
-
-                     if (charrampointer=19) then
-                        setline <= '1';
-                        linenum <= 1;
-
-                     elsif (charrampointer=39) then
-                        setline <= '1';
-                        linenum <= 0;
-                     end if;
-
-                     if (charrampointer=char_ram_type'high) then
-                        charrampointer <= 0;
-                     else
-                        charrampointer <= charrampointer + 1;
-                     end if;
-
-                  end if;
-
-                  lcde  <= '1';
-                  state <= stage3;
-
-            -- clear lcde (latching data)
-               when stage3 =>
-                  lcde  <= '0';
-                  state <= delay;
-
-            -- provide delay to allow instruciton to execute
-               when delay =>
-                  if (timer=0) then
-                     state <= stage1;
-                  else
-                     timer <= timer - 1;
-                  end if;
-
-            end case;
-         end if;
-      end if;
-   end process;
-
-end structural;
